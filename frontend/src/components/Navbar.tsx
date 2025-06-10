c549bbea018ebdd7c1255effd2e0a6b6bf09c9dc
William Inyam <sw.inyam@outlook.com>
2025-06-10 15:24:31 +0000

    :up: update: add theme selector to navbar

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,7 +1,33 @@
+import { ContentWrapper } from "@/components/index";
+import { SunMedium, Moon } from "lucide-react";
+import { UseTheme } from "@/hooks/ThemeProvider";
+import { Button } from "@/components/ui/button";
+
 const AppNavBar = () => {
+  const { theme, setTheme } = UseTheme();
+  const toggleTheme = () => {
+    setTheme(theme === "light" ? "dark" : "light");
+    window.localStorage.setItem("login_ui_theme", theme);
+  };
+
   return (
-    <div>
-      <p>Navbar</p>
+    <div
+      className="p-1 bg-neutral-200 dark:bg-neutral-800 dark:text-neutral-100"
+      onClick={toggleTheme}
+      data-theme={theme}
+    >
+      <ContentWrapper className="flex justify-center items-center">
+        <div className="cursor-pointer rounded-sm p-2">
+          <Button
+            asChild
+            size="icon"
+            className="p-1 duration-500 transition-all dark:hover:text-neutral-700"
+            variant="ghost"
+          >
+            {theme === "light" ? <SunMedium /> : <Moon />}
+          </Button>
+        </div>
+      </ContentWrapper>
     </div>
   );
 };
