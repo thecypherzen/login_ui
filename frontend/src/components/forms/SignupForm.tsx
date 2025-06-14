8951e1e672874534a37f54248f98ef619a6e03bd
William Inyam <sw.inyam@outlook.com>
2025-06-14 02:36:15 +0000

    :star: feat: complete main signup form logic

--- frontend/src/components/forms/SignupForm.tsx
+++ frontend/src/components/forms/SignupForm.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { z } from "zod";
@@ -12,8 +13,11 @@ import {
 import { Input } from "@/components/ui/input";
 import type { Control, FieldPath } from "react-hook-form";
 import { Button } from "@/components/ui/button";
-import { Link } from "react-router";
+import { Link, useNavigate } from "react-router";
 import { cn } from "@/lib/utils";
+import { Spinner } from "@/components/index";
+import { capitalCase } from "change-case";
+import axios from "axios";
 
 // form schema
 const formSchema = z.object({
@@ -32,6 +36,13 @@ const formSchema = z.object({
 
 // Form component
 const LoginForm: React.FC<LoginFormPropsType> = ({ className }) => {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [showMessage, setShowMessage] = useState<boolean>(false);
+  const [message, setMessage] = useState<FormMessageType | undefined>(
+    undefined,
+  );
+  const navigate = useNavigate();
+  const delay = 1000;
   const generatedForm = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -40,8 +51,57 @@ const LoginForm: React.FC<LoginFormPropsType> = ({ className }) => {
     },
   });
 
-  const formOnSubmit = (values: z.infer<typeof formSchema>) => {
-    console.log(values);
+  const hideSpinner = () => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, delay);
+  };
+
+  const clearError = () => {
+    setShowMessage(false);
+    setMessage(undefined);
+  };
+
+  const formOnSubmit = async (values: z.infer<typeof formSchema>) => {
+    clearError();
+    setIsLoading(true);
+    const baseUrl =
+      import.meta.env.VITE_NODE_ENV === "dev"
+        ? `${import.meta.env.VITE_API_DEV_BASE_URL}`
+        : import.meta.env.VITE_API_LIVE_BASE_URL;
+    try {
+      const res = await axios.post(
+        `${baseUrl}/auth/signup`,
+        { ...values },
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        },
+      );
+      console.log(res);
+      setMessage({
+        type: "success",
+        message: "Account created. Taking you to login page",
+      });
+      setTimeout(() => {
+        setShowMessage(true);
+        setTimeout(() => {
+          navigate("/");
+        }, 1500);
+      }, delay);
+    } catch (err: Error | any) {
+      console.error(err);
+      setMessage({
+        type: "error",
+        message: `${err?.response?.data?.message ?? "Sign up failed for some reason"}.`,
+      });
+      setTimeout(() => {
+        setShowMessage(true);
+      }, delay);
+    } finally {
+      hideSpinner();
+    }
   };
 
   return (
@@ -57,6 +117,7 @@ const LoginForm: React.FC<LoginFormPropsType> = ({ className }) => {
             inputType="email"
             control={generatedForm.control}
             label="Username"
+            disabled={isLoading}
           />
         </div>
         <div>
@@ -66,10 +127,33 @@ const LoginForm: React.FC<LoginFormPropsType> = ({ className }) => {
             inputType="password"
             control={generatedForm.control}
             label="Password"
+            disabled={isLoading}
           />
         </div>
+        {showMessage && message && (
+          <div>
+            <p
+              className={cn(
+                "text-sm px-1",
+                message.type === "error"
+                  ? "text-destructive"
+                  : message.type === "success"
+                    ? "text-green-500"
+                    : "text-neutral-300 dark:text-neutral-200",
+              )}
+            >
+              <strong>{capitalCase(message.type)}:&nbsp;</strong>
+              {message.message}
+            </p>
+          </div>
+        )}
         <div className="flex flex-col gap-y-4 items-center justify-between md:flex-row text-sm lg:text-base">
           <Button className="cursor-pointer dark:bg-neutral-200 dark:hover:bg-neutral-500 dark:text-neutral-900 w-full md:w-auto">
+            {isLoading && (
+              <span className="space-x-2">
+                <Spinner />
+              </span>
+            )}
             Create Account
           </Button>
           <p className="text-neutral-500 flex flex-col gap-y-1 text-center md:flex-row md:text-left">
@@ -97,6 +181,7 @@ const LoginFormField: React.FC<LoginFormFieldPropsType> = ({ ...props }) => {
           <FormLabel>{props.label}</FormLabel>
           <FormControl>
             <Input
+              disabled={props.disabled}
               placeholder={props.placeholder}
               {...field}
               type={props.inputType}
@@ -126,6 +211,13 @@ type LoginFormFieldPropsType = {
   description?: string;
   label?: string;
   inputClassName?: string;
+  disabled?: boolean;
+};
+
+type FormMessageType = {
+  type: "success" | "error" | "warning" | "neutral";
+  message: string;
 };
 
 export default LoginForm;
+export type { FormMessageType };
