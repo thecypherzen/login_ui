ddc9e1aac282161baa3debaddd6b564bce6ce1a5
William Inyam <sw.inyam@outlook.com>
2025-06-14 04:45:40 +0000

    :star::up: feat: enable theme style in dashboard

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,9 +1,15 @@
 /**
  * User dashboard component
  */
+import { UseTheme } from "@/hooks/ThemeProvider";
+
 const Dashboard = () => {
+  const { theme } = UseTheme();
   return (
-    <div className="min-h-[100svh] flex justify-center p-6 bg-neutral-100 dark:bg-neutral-800">
+    <div
+      className="min-h-[100svh] flex justify-center p-6 bg-neutral-100 dark:bg-neutral-800"
+      data-theme={theme}
+    >
       <h2 className="font-bold text-xl md:text-2xl dark:text-neutral-100">
         Dashboard
       </h2>
